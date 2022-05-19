802cf2a638898199de4f1c6c6f72c4ae85b900c9
hlj <hulj3@yonghui.cn>
2022-05-19 17:04:05 +0800

    fix: test

---
+++ index.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 胡路杰
+ * @Date: 2022-05-19 17:03:16
+ * @Descripttion: 
+ * @Company: YH
+ */
+import React from "react";
+
+const name = () => {
+  console.log(111)
+
+  return (
+    <div>123131</div>
+  )
+}
+
+export default name
